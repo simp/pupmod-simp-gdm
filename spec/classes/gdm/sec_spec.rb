9a02e091e670119fafb8619519446268f34a85c4
Liz Nemsick <lnemsick.simp@gmail.com>
2016-11-11 17:01:09 -0500

    (SIMP-1677) Remove deprecated Puppet.newtype
    Removed unused gconf type.
    
    SIMP-1892 #close

--- spec/classes/gdm/sec_spec.rb
+++ spec/classes/gdm/sec_spec.rb
@@ -9,10 +9,6 @@ describe 'xwindows::gdm::sec' do
 
   it { is_expected.to create_class('xwindows::gdm::sec') }
 
-#  it { should contain_gconf('banner_message_enable') }
-#  it { should contain_gconf('banner_message_text') }
-#  it { should contain_gconf('disable_user_list') }
-
   @set_these = [
     'daemon_chooser', 'daemon_remote_greeter', 'daemon_standard_x_server', 'disable_chooser_button',
     'standard_server_priority', 'terminal_server_flex', 'terminal_server_handled'
